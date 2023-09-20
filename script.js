1267fc9296041b40778e402f93678431eca9fb1e
Valène Bonnet <bonnet.valene@gmail.com>
2023-09-20 12:44:59 +0200

    la route

--- script.js
+++ script.js
@@ -4,20 +4,24 @@
 
 // Search button: <button type="button" id="btn-search" class="element-travel-search">
 
+console.log("---");
+
 document.querySelector("#btn-search").addEventListener("click", function () {
-  fetch("http://localhost:3000/")
+  fetch("http://localhost:3000/trips")
     .then((response) => response.json())
     .then((data) => {
-      if (data.result) {
+      console.log(data);
+      console.log("---");
+      if (data) {
         let htmlData = "";
         data.forEach((item) => {
           htmlData += `
-        <div class="element-travel-book">
-              <div id="voyage">${departure} > ${arrival}</div>
-              <div id="heure">${date}</div>
-              <div id="prix">${price}</div>
+            <div class="element-travel-book">
+              <div id="voyage">${item.departure} > ${item.arrival}</div>
+              <div id="heure">${item.date}</div>
+              <div id="prix">${item.price}</div>
               <button type="button" class="btn-book">Book</button>
-            </div>        
+            </div>
         `;
         });
         document.querySelector("#display-search").innerHTML = htmlData;
